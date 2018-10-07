61c8cd14f73b506f37f81e08ae23e2f396a4ed2a
jerry <90mlee@gmail.com>
2018-10-07 17:01:00 +0900

    요다 Reg_v1

--- app/src/main/java/blockChainTCL/babychain/RegActivity.java
+++ app/src/main/java/blockChainTCL/babychain/RegActivity.java
@@ -1,7 +1,12 @@
 package blockChainTCL.babychain;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class RegActivity extends Activity {
 
@@ -10,4 +15,38 @@ public class RegActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
     }
+
+
+    public void mOnClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btnBack:
+                intent = new Intent(this, MainActivity.class);
+                break;
+            case R.id.btnReg:
+                //intent = new Intent(this, InqActivity.class);
+
+                new AlertDialog.Builder(this)
+                        .setTitle("확인")
+                        .setMessage("등록하시겠습니까?")
+                        .setIcon(android.R.drawable.ic_menu_save)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // 확인시 처리 로직
+                                Toast.makeText(RegActivity.this, "등록을 완료했습니다.", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }})
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // 취소시 처리 로직
+                                Toast.makeText(RegActivity.this, "취소하였습니다.", Toast.LENGTH_SHORT).show();
+                            }})
+                        .show();
+                break;
+        }
+        if(intent != null) {
+            startActivity(intent);
+        }
+    }
+
 }
