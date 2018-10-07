b31c7f105a1610aab39d1144554c365f907af67f
dmi77 <dmi77@naver.com>
2018-10-07 19:49:03 +0900

    코밋

--- app/src/main/java/blockChainTCL/babychain/ModActivity.java
+++ app/src/main/java/blockChainTCL/babychain/ModActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static blockChainTCL.babychain.R.id.modVal;
 
 public class ModActivity extends Activity {
 
@@ -14,15 +18,23 @@ public class ModActivity extends Activity {
     }
 
     public void mOnClick(View v) {
-
         Intent intent = null;
+        TextView showVal = (TextView)findViewById(R.id.valueTextView);
+        EditText updateVal = (EditText)findViewById(R.id.modVal);
+
         switch (v.getId()) {
             case R.id.backButton:
                 intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
                 break;
-        }
-        if(intent != null) {
-            startActivity(intent);
+
+            case R.id.inqButton:
+                showVal.setText("010-3456-7890");
+                break;
+
+            case R.id.modButton:
+                showVal.setText(updateVal.getText());
+                break;
         }
     }
 }
