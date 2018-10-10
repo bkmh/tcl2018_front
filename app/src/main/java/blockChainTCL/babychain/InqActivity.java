b2b0e505c350ad9d29dee41adf76b43e800c32e7
hrim <hrpark13@gmail.com>
2018-10-10 22:06:30 +0900

    듀

--- app/src/main/java/blockChainTCL/babychain/InqActivity.java
+++ app/src/main/java/blockChainTCL/babychain/InqActivity.java
@@ -1,7 +1,11 @@
 package blockChainTCL.babychain;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InqActivity extends Activity {
 
@@ -10,4 +14,24 @@ public class InqActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inq);
     }
+
+    public void mOnClick(View v) {
+
+        Intent intent = null;
+        TextView showVal = (EditText)findViewById(R.id.editText6);
+
+        switch (v.getId()) {
+            case R.id.backButton:
+                intent = new Intent(this, MainActivity.class);
+                break;
+            case R.id.inqButton:
+                showVal.setText("조회되었습니다.");
+                break;
+        }
+        if(intent != null) {
+            startActivity(intent);
+        }
+    }
+
+
 }
