66ad3c1a4d46d3e0aec2b15d49689b39b1c7a38f
DragonBong <dragonbong0614@gmail.com>
2018-10-14 20:24:17 +0900

    약관 페이지(더미) 추가

---
+++ app/src/main/java/blockChainTCL/babychain/Cache.java
@@ -0,0 +1,61 @@
+package blockChainTCL.babychain;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Cache {
+
+    private Context context;
+
+    public Cache(Context co){
+        context = co;
+    }
+
+    private File getCacheDir(Context context) {
+//        File cacheDir = null;
+//        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+//            cacheDir = new File(Environment.getExternalStorageDirectory(), "cachefolder");
+//            if(!cacheDir.isDirectory()) {
+//                cacheDir.mkdirs();
+//            }
+//        }
+//        if(!cacheDir.isDirectory()) {
+//            cacheDir = context.getCacheDir();
+//        }
+//        return cacheDir;
+        return context.getCacheDir();
+    }
+
+    public void writeCache(String obj, String fileName) throws IOException {
+        File cacheDir = getCacheDir(context);
+        File cacheFile = new File(cacheDir, fileName);
+        if(!cacheFile.exists()) {
+            cacheFile.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(cacheFile);
+        fileWriter.write(obj);
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    public String readCache(String fileName) throws IOException {
+        File cacheDir = getCacheDir(context);
+        File cacheFile = new File(cacheDir, fileName);
+        if(!cacheFile.exists()) {
+            cacheFile.createNewFile();
+        }
+        FileInputStream inputStream = new FileInputStream(cacheFile);
+        Scanner s = new Scanner(inputStream);
+        StringBuilder sb= new StringBuilder();
+        while(s.hasNext()){
+            sb.append(s.nextLine());
+        }
+        inputStream.close();
+        return sb.toString();
+    }
+}
--- app/src/main/java/blockChainTCL/babychain/MainActivity.java
+++ app/src/main/java/blockChainTCL/babychain/MainActivity.java
@@ -3,29 +3,75 @@ package blockChainTCL.babychain;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+
+    private BackPressCloseHandler backPressCloseHandler;
+    private static String AGREE_CACHE_NAME = "fileAgree";
+    private static String AGREED = "agreed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try {
+            Cache cache = new Cache(this);
+
+            if(AGREED.equals(cache.readCache(AGREE_CACHE_NAME))) {
+                setContentView(R.layout.activity_main);
+            } else {
+                setContentView(R.layout.activity_terms);
+            }
+        } catch (IOException e) {
+            setContentView(R.layout.activity_terms);
+        }
+
+        // 약관 1 스크롤 기능 추가
+        TextView textTerms1Boby = (TextView)findViewById(R.id.textTerms1Boby);
+        if(textTerms1Boby != null) {
+            textTerms1Boby.setMovementMethod(new ScrollingMovementMethod());
+        }
+
+        backPressCloseHandler = new BackPressCloseHandler(this);
     }
 
     public void mOnClick(View v) {
-
         Intent intent = null;
         switch (v.getId()) {
+            // 약관 '동의 및 진행' 버튼 클릭 시
+            case R.id.agreeButton:
+                try {
+                    Cache cache = new Cache(this);
+                    cache.writeCache(AGREED, AGREE_CACHE_NAME);
+
+                    setContentView(R.layout.activity_main);
+                } catch (IOException e) {
+                    this.finish();
+                }
+                break;
+
+            // 메인메뉴 '등록' 버튼 클릭 시
             case R.id.regButton:
                 intent = new Intent(this, RegActivity.class);
                 break;
+
+            // 메인메뉴 '조회' 버튼 클릭 시
             case R.id.inqButton:
                 intent = new Intent(this, InqActivity.class);
                 break;
+
+            // 메인메뉴 '변경' 버튼 클릭 시
             case R.id.modButton:
                 intent = new Intent(this, ModActivity.class);
                 break;
+
+            // 메인메뉴 '임시' 버튼 클릭 시
             case R.id.tempButton:
                 intent = new Intent(this, TempActivity.class);
                 break;
@@ -33,5 +79,11 @@ public class MainActivity extends Activity {
         if(intent != null) {
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed(); // 뒤로 가기 입력 시 바로 종료
+        //backPressCloseHandler.onBackPressed(); // 뒤로 가기 두번 눌러 종료
     }
 }
